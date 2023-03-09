18c4479e9bb042bd970d8bc994bcf04834a1edf1
Foxyrhei <124771564+Foxyrhei@users.noreply.github.com>
2023-03-09 16:46:05 +0200

    Chapter 4 Exercise 20

---
+++ src/Chapter4/CH_4_20.java
@@ -0,0 +1,68 @@
+package Chapter4;
+/*
+(Salary Calculator) Develop a Java application that determines the gross pay for each of
+three employees. The company pays straight time for the first 40 hours worked by each employee
+and time and a half for all hours worked in excess of 40.
+You’re given a list of the employees, their number of hours worked last week and their hourly rates.
+Your program should input this information for each employee,
+then determine and display the employee’s gross pay.
+Use class Scanner to input the data.
+ */
+
+import java.util.Scanner;
+
+public class CH_4_20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String employeeName;
+        double hoursWorked;
+        double rate = 0;
+        double overtime;
+        double grossPay;
+        int employeeCounter = 0;
+
+
+        System.out.printf("Enter employee name%n");
+        employeeName = input.next();
+
+        System.out.printf("Enter number of hours worked last week %n(type -1 to quit)%n");
+        hoursWorked = input.nextDouble();
+
+
+        while ((hoursWorked != -1) && (rate != -1)){
+
+            System.out.printf("Enter hourly rate %n(type -1 to quit)%n");
+            rate = input.nextDouble();
+
+            if (hoursWorked > 40) {
+                overtime = hoursWorked - 40;
+                grossPay = (40 * rate) + (overtime * 1.5 * rate);
+                System.out.printf("Employee's gross pay is: %.2f%n", grossPay);
+
+                System.out.printf("Enter employee name%n");
+                employeeName = input.next();
+
+                System.out.printf("Enter number of hours worked last week %n(type -1 to quit)%n");
+                hoursWorked = input.nextDouble();
+
+            }
+            else if (hoursWorked <= 40){
+                grossPay = hoursWorked * rate;
+                System.out.printf("Employee's gross pay is: %.2f%n", grossPay);
+                System.out.printf("Enter employee name%n");
+                employeeName = input.next();
+
+                System.out.printf("Enter number of hours worked last week %n(type -1 to quit)%n");
+                hoursWorked = input.nextDouble();
+            }
+            else {
+                System.out.printf("No data entered");
+            }
+        }
+
+
+
+
+    }
+}
