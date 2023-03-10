ceb744e714d6969fb271f4c128961c53464c483f
Foxyrhei <124771564+Foxyrhei@users.noreply.github.com>
2023-03-10 21:08:40 +0200

    Chapter 4 Exercise 29 (formatting)

--- src/Chapter4/CH_4_29.java
+++ src/Chapter4/CH_4_29.java
@@ -4,7 +4,9 @@ package Chapter4;
 of a square, then displays a hollow square of that size made of asterisks. Your program should work
 for squares of all side lengths between 1 and 20.
  */
+
 import java.util.Scanner;
+
 public class CH_4_29 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -15,8 +17,8 @@ public class CH_4_29 {
         System.out.printf("Enter square size: %n");
         int n = input.nextInt();
 
-        for(columnCount = 1; columnCount <= n; columnCount++){
-            for(rowCount = 1; rowCount <= n; rowCount++){
+        for (columnCount = 1; columnCount <= n; columnCount++) {
+            for (rowCount = 1; rowCount <= n; rowCount++) {
                 System.out.printf("*");
             }
             System.out.printf("\n");
