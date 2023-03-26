7f76b1d56ae15f07161b5a713e9113a8dadc18b3
Foxyrhei <124771564+Foxyrhei@users.noreply.github.com>
2023-03-26 19:31:10 +0300

    Chapter 6 Exercise 20

---
+++ src/Chapter6/CH_6_20.java
@@ -0,0 +1,24 @@
+package Chapter6;
+
+import java.util.Scanner;
+
+/*
+(Circle Area) Write an application that prompts the user for the radius of a circle and uses
+a method called circleArea to calculate the area of the circle.
+ */
+public class CH_6_20 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Enter radius: ");
+        double r = input.nextDouble();
+
+        System.out.printf("Circle area is: %f", circleArea(r));
+    }
+
+    public static double circleArea(double r) {
+        double area = Math.PI * Math.pow(r, 2);
+        return area;
+    }
+}
