89920518bf89ab2b483400802b52ba9de993c374
Foxyrhei <124771564+Foxyrhei@users.noreply.github.com>
2023-03-30 17:16:26 +0300

    Chapter 5 Exercise 14 - reworked with additional columns

--- src/Chapter5/CH_5_14.java
+++ src/Chapter5/CH_5_14.java
@@ -8,24 +8,22 @@ vary the interest rate
 public class CH_5_14 {
 
     public static void main(String args[]) {
-        double amount; // amount on deposit at end of each year
+        double amount = 0; // amount on deposit at end of each year
         double principal = 1000.0; // initial amount before interest
         double rate; // interest rate
 
         // display headers
-        System.out.printf("%s\t%s%20s%n", "Year", "Rate", "Amount on deposit");
+        System.out.printf("%10s%16s%16s%16s%16s%16s%17ss%n", "Year", "Rate 5%", "Rate 6%", "Rate 7%", "Rate 8%", "Rate 9%", "Rate 10%");
 
-        for (rate = 0.05; rate <= 0.1; rate += 0.01) {
-
-            // calculate amount on deposit for each of ten years
-            for (int year = 1; year <= 10; year++) {
+        // calculate amount on deposit for each of ten years
+        for (int year = 1; year <= 10; year++) {
+            System.out.printf("%10d", year);
+            for (rate = 0.05; rate <= 0.1; rate += 0.01) {
                 // calculate new amount for specified year
                 amount = principal * Math.pow(1.0 + rate, year);
-
-                // display the year and the amount
-                System.out.printf("%d\t\t%.2f\t\t%.2f%n", year, rate, amount);
+                System.out.printf("%16.2f", amount);
             }
+            System.out.println();
         }
     }
-
 }
